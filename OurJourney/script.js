258d62367f04a68ab41a5a06f2361c75c9f6dc06
IanHendrix <90948154+iiiyeahnd@users.noreply.github.com>
2026-03-04 15:53:53 +0800

    Delete OurJourney directory

--- OurJourney/script.js
+++
@@ -1,9 +0,0 @@
-const playBtn = document.getElementById('playBtn');
-
-// Click animation: briefly add 'clicked' class, then remove it
-playBtn.addEventListener('click', function () {
-  playBtn.classList.add('clicked');
-  setTimeout(function () {
-    playBtn.classList.remove('clicked');
-  }, 200);
-});
